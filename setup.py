d811eb8c5f9eac088279001e80e84bd088c253ea
Huszár Csaba Benedek <54895209+huszarcs@users.noreply.github.com>
2025-02-14 10:30:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
         'pandas',
         'diffdist>=0.1',
         'Cython>=0.29.30',
-        'faiss-gpu>=1.7.2',
+        'faiss-cpu',
         'gdown>=4.7.1',  # 'libmr>=0.1.9'
     ],
     classifiers=[
